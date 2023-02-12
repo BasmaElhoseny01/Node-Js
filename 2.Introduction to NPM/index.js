9e0c860bd84ed1969e0527663966ee94078514ef
BasmaElhoseny <basmaelhoseny6@gmail.com>
2023-02-12 08:22:03 +0200

    Update index.js

--- 2.Introduction to NPM/index.js
+++ 2.Introduction to NPM/index.js
@@ -1,3 +1,5 @@
+//@ beginning of every project type npm init
+
 //To run nodemon npm run start as script added in package.json
 // 1) core modules
 const fs= require('fs')
@@ -12,14 +14,3 @@ const slugify =require("slugify")
 const func=require('./module')
 
 console.log(slugify('Fresh Fruits',{lower:true}))
-//Npm is a command line interface that comes automatically with Node js to manage open source packages 
-//@ beginning of every project type npm init
-
-// Types of packages that can be installed 
-// 1. Simple Dependencies ==> npm install slugify
-// 2. Development Dependencies (Tools for development like debugger or testing library) ==> npm install nodemon --save-dev
-
-
-// Types of installation
-// 1.Local package is  available only in this project ⇒ to be able to run it from command line you need to specify a script "start":"nodemon index.js" ==> npm run start
-// 2.Globally so dependency is installed globally in the machine and can be used from every where, npm install nodemon —global. ⇒ you may got errors due to permissions you only need to open it :)
